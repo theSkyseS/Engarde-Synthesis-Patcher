609e55f9c78a78255f5abc18f99e240e73baef18
Бугаев Андрей <bugaev.andrei55@gmail.com>
2021-03-21 17:01:22 +0600

    created DTO.cs

---
+++ Engarde_Synthesis/DTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Engarde_Synthesis
+{
+    public class Config
+    {
+        [JsonProperty("behaviour")]
+        public string Behaviour { get; set; } = string.Empty;
+
+        [JsonProperty("RaceData")]
+        public Dictionary<string, RaceData> RaceData { get; set; } = new();
+    }
+
+    public class RaceData
+    {
+        [JsonProperty("Unarmed Reach")] public float UnarmedReach { get; set; } = float.MaxValue;
+        [JsonProperty("Angular Acceleration")] public float AngularAcceleration { get; set; } = float.MaxValue;
+        [JsonProperty("Angle Tolerance")] public float AngleTolerance { get; set; } = float.MaxValue;
+
+        [JsonProperty("Attacks")]
+        public Dictionary<string, Attack> Attacks { get; set; } = new Dictionary<string, Attack>();
+    }
+
+    public class Attack
+    {
+        [JsonProperty("Strike Angle")] public float StrikeAngle { get; set; } = float.MaxValue;
+        [JsonProperty("Attack Angle")] public float AttackAngle { get; set; } = float.MaxValue;
+    }
+}
