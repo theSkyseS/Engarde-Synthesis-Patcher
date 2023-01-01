c5fbe91399450a2b7391f957148a78e4dfe75430
zackmon99 <monzack4@gmail.com>
2022-12-31 20:05:17 -0700

    adding settings

--- Engarde_Synthesis/Settings/BasicAttacks.cs
+++ Engarde_Synthesis/Settings/BasicAttacks.cs
@@ -6,8 +6,10 @@ namespace Engarde_Synthesis.Settings
     {
         [SynthesisSettingName("Enhanced normal attacks and controls")]
         public bool basicAttackTweaks = true;
-        //[SynthesisSettingName("Include Dual Wield tweaks")]
-        //public bool dwAttackTweaks = true;
+        [SynthesisSettingName("Include Dual Wield tweaks")]
+        public bool dwAttackTweaks = true;
+        [SynthesisSettingName("Alternate swings for Dual Wield")]
+        public bool dwAlternateSwings = true;
         [SynthesisSettingName("Include Unarmed tweaks")]
         public bool h2HAttackTweaks = true;
         [SynthesisSettingName("Spellsword can block")]
